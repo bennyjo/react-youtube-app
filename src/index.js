527010ec970f6726cf775e54c5cdb0fbe6253ba0
Benny Johansson <benny.json@gmail.com>
2017-10-22 10:40:53 +0100

    feat(app): fetch youtube videos

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSeach from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyBxPrIEahb8mZO9XQ4T0Jwavt2R1r5_LaY';
 
-// Create a new component.
-// This component should produce some HTMl
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = { videos: [] };
+
+        this.fetchVideos();
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+    
+    fetchVideos() {
+        YTSeach({ key: API_KEY, term: 'surfboards'}, videos => {
+            this.setState({ videos });
+        });
+    }
 }
 
-// Take this component's generated HTML
-// and put it in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
